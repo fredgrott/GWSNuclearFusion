72f22a5e0a454d3c1fc52bbf702e86c99f161620
Fred Grott <fred.grott@gmail.com>
2015-12-19 07:55:59 -0600

    added more executor code

---
+++ library/src/main/java/com/github/shareme/gwsnuclearfusion/library/executor/AbstractInteractor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014 AdriÃ¡n GarcÃ­a Lomas
+ * Modifications Copyright(C) 2015 Fred Grott(GrottWorkShop)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.github.shareme.gwsnuclearfusion.library.executor;
+
+/**
+ * AbstractInteractor
+ * Created by fgrott on 12/19/2015.
+ */
+@SuppressWarnings("unused")
+public abstract class AbstractInteractor implements Interactor {
+
+    private InteractorExecutor interactorExecutor;
+    private MainThreadExecutor mainThreadExecutor;
+
+    public AbstractInteractor(InteractorExecutor interactorExecutor, MainThreadExecutor mainThreadExecutor) {
+        this.interactorExecutor = interactorExecutor;
+        this.mainThreadExecutor = mainThreadExecutor;
+    }
+
+    public InteractorExecutor getInteractorExecutor() {
+        return interactorExecutor;
+    }
+
+    public void setInteractorExecutor(InteractorExecutor interactorExecutor) {
+        this.interactorExecutor = interactorExecutor;
+    }
+
+    public MainThreadExecutor getMainThreadExecutor() {
+        return mainThreadExecutor;
+    }
+
+    public void setMainThreadExecutor(MainThreadExecutor mainThreadExecutor) {
+        this.mainThreadExecutor = mainThreadExecutor;
+    }
+}
